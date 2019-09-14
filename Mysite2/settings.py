0261a1cc0616c2ce6dcafacd713f28451d08eb9d
Orlando Castilla <37159095+castillag18@users.noreply.github.com>
2019-09-14 14:05:15 -0500

    Update settings.py

--- Mysite2/settings.py
+++ Mysite2/settings.py
@@ -74,15 +74,11 @@ WSGI_APPLICATION = 'Mysite2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'djangodb',  
-        'USER':'root',  
-        'PASSWORD':'mysql',  
-        'HOST':'localhost',  
-        'PORT':'3306'  
-    }  
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }  
 
 
